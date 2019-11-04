d2506f909af24db442aeb6a6952bccb9d36b71af
Gessica Andrade <gessik.danielly@gmail.com>
2019-11-04 16:36:02 -0300

    Nodemon e Sucrase

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,8 +1,8 @@
-const { Router } = require('express');
+import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
   res.json('Hello Word');
 })
 
-module.exports = routes;
+export default routes;
