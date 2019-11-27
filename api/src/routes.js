f13d60e8ceed00c1bbac16cd4af6dfecb96b91be
Gessica Andrade <gessicadrandrade@gmail.com>
2019-11-26 21:57:02 -0300

    Middlewares end Validations

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
@@ -10,5 +11,8 @@ routes.get('/', (req, res) => {
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
